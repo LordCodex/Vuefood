4eb3766fa023b17c18641ae4aaa4caa4f0b1c03f
Olaitan Michael Olanrewaju <michaelolaitan6@gmail.com>
2022-05-07 11:26:58 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavBar/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavBar,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+   padding: 0;
+    margin: 0;
+    text-decoration: none;
+    list-style-type: none;
+    box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    
 }
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,64 @@
+<template >
+  <div v-bind:class="'hero-section'">
+    <div v-bind:class="'nav-bar'">
+      <div class="logo">
+            <h1> &#9776; VUE-FOOD</h1>
+        </div>
+        <div class="navs-2">
+      <div class="nav-links">
+            <ul>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Service</a>
+            </li>
+            <li>
+                <a href="#">Portfolio</a>
+            </li>
+            <li>
+                <a href="#">Pricing</a>
+            </li>
+            <li>
+                <a href="#">Team</a>
+            </li>
+            <li>
+                <a href="#">Contact</a>
+            </li>
+            </ul>
+      </div>
+      <div class="nav-icon">
+         <h2> &#9776;</h2>
+        </div>
+        </div>
+    </div>
+    <div v-bind:class="'hero-content'">
+        <div class="hero-text">
+    <h1>Minimal  Food Website <br> Vue Template By Codex UI.</h1>
+    <br>
+    <p>We are a digital agency that helps brands to achieve their business outcomes. We see technology as a
+                    tool to create amazing things.</p>
+                    <br>
+                    <div class="hero-button">
+    <button  v-bind:class="'btn'" v-on="btn">ORDER NOW</button>
+    <h4> <b>➧</b>  Watch Intro</h4>
+    </div>
+        </div>
+        <div class="hero-image">
+            <img src="https://nextfood.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbanner2.8cde3dea.jpg&w=1920&q=75" alt="" style="border-radius: .5em;">
+        </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style scoped>
+@import "../assets/Header.css";
+
+</style>
